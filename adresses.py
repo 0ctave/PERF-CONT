f9a5e48c37f350ecf17f414651777827e26b3b7d
gloxounet <matteo.glock@laposte.net>
2022-10-12 10:35:48 +0200

    paths

--- adresses.py
+++ adresses.py
@@ -3,5 +3,16 @@ ports = {
         'rest':'3001',
         'grpc':'3101',
         'graphql':'3201',
+    },
+}
+
+paths = {
+    'rest':{
+        'url':'/movies/',
+        'method':'GET'
+    },
+    'graphql':{
+        'url':'/graphql',
+        'method':'POST'
     }
 }
